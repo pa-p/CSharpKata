e4dc1ce04b4829b9b2b66acaaf73f237df233ca9
Pawel P <pawlukojc.p@gmail.com>
2024-02-29 23:56:51 +0100

    PaperScissorsRock added tests.

---
+++ PaperScissorsRock.Tests/ForcedPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperScissorsRock.Tests
+{
+    public class ForcedPlayer : IPlayer
+    {
+        private Choice _choice;
+
+        public ForcedPlayer(Choice choice)
+        {
+            _choice = choice;
+        }
+        public Choice GetChoice()
+        {
+            return _choice;
+        }
+    }
+}
---
+++ PaperScissorsRock.Tests/PSRTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperScissorsRock.Tests
+{
+    public class PSRTests
+    {
+        [Test]
+        public void RockBeatsScissorsPlayer1Wins()
+        {
+            GameManagerDI gm = new GameManagerDI(new ForcedPlayer(Choice.Rock), new ForcedPlayer(Choice.Scissors));
+
+            Assert.That(gm.PlayRound(), Is.EqualTo(RoundResult.Player1Win));
+        }
+
+        [Test]
+        public void RockBeatsScissorsPlayer2Wins()
+        {
+            GameManagerDI gm = new GameManagerDI(new ForcedPlayer(Choice.Scissors), new ForcedPlayer(Choice.Rock));
+
+            Assert.That(gm.PlayRound(), Is.EqualTo(RoundResult.Player2Win));
+        }
+    }
+}
