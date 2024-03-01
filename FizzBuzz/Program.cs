b1cd0b29467adaf7f21a4974b1f9fd96c06f9557
Pawel P <pawlukojc.p@gmail.com>
2024-03-01 07:37:01 +0100

    FizzBuzz coffee time exercise ;)

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * FizzBuzz
+ * Print numbers from 1 to 100
+ * if a number is a factor of 3 print Fizz
+ * if a number is a factor of 5 pront Buzz
+ * if a number is a factor of 3 and 5 print FizzBuzz
+ * in other cases print number
+ */
+
+using System.Collections;
+
+var numbers = Enumerable.Range(1, 100);
+foreach (var number in numbers)
+{
+    if (number % 3 == 0 && number % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+        continue;
+    }
+    else if (number % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+        continue;
+    }
+    else if (number % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+        continue;
+    }
+    else
+    {
+        Console.WriteLine($"{number}");
+    }
+}
