19a31fa3da02350d280d3668fac1efce6a30e740
Pawel P <pawlukojc.p@gmail.com>
2024-03-22 10:53:24 +0100

    Reverse words exercise.

---
+++ ReverseString/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+ * Reverse words in a string without leading trailing and multiplied space.
+ */
+
+using ReverseString;
+
+var txt1 = "  The   sky is                     blue  ";
+var txt2 = txt1.ReverseWords();
+
+Console.WriteLine(txt1);
+Console.WriteLine(txt2);
---
+++ ReverseString/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    public static class StringExtensions
+    {
+        public static string ReverseWords(this string s)
+        {
+            if (s is null)
+                return "";
+
+            if (s.Length == 0) 
+                return s;
+
+            var words = s.Split(" ");
+                                  
+            return string.Join(
+                " ",
+                words.Where(w => !string.IsNullOrWhiteSpace(w)).Reverse());
+        }
+    }
+}
